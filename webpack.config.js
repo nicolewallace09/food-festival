ef8457e7a153ffc2192cfeafefc34b80a433dc01
nicolewallace09 <nicole.elisaw@gmail.com>
2020-07-15 22:05:03 -0700

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,54 @@
 const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 // webpack properties: entry, output, and mode 
-
+// modifying for multiple entry points - for each of our pages 
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+        app: './assets/js/script.js',
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        // build will create a series of bundled files for each entry object 
+        filename: '[name].bundle.js'
+    },
+    // file-loader for images 
+    module: {
+        rules: [
+          {
+            // looking for a regex for image files with extension .jpg
+            test: /\.jpg$/i,
+            use: [
+                {
+                    loader: "file-loader",
+                    options: {
+                      name (file) {
+                        return "[path][name].[ext]"
+                      },
+                      publicPath: function(url) {
+                          return url.replace("../", "/assets/")
+                      }
+                    }
+                 },
+                 {
+                    loader: 'image-webpack-loader'
+                  }
+            ]
+          }
+        ],
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery", 
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
     ],
     mode: 'development',
 
